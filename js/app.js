aa805d4ce8d89a5b0ddd69427be1db8e61c84949
Kiran Bhat Kashi <rashmihv88@gmail.com>
2018-06-25 20:05:55 -0400

    added game functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,t) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    //initialize x-0,y=145
+    //Y values :-225(last row),60/65(first row),145(middle row)
+    //X value :-0,(first col) ,100 (2 column),200(3rd),300,400
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.t = t;//speed
+    this.reset = false;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +21,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //console.log("update prototype of enemy"+dt);
+    if(this.x >= 0 && this.x <= 400) {
+        this.x += dt * this.t;
+    }
+    else {
+        this.x = 0;    
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,16 +38,210 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    //Y values :- max 400 ,min 0/-10(water)
+    //X value :-0,(first col) ,100 (2 column),200(3rd),300,400
+    this.x = 150;
+    this.y = 350;
+    this.lives = 10;
+};
 
+var player = new Player();
+
+Player.prototype.update = function() {};
+
+//To draw the player on screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Player movement,increases by +50
+Player.prototype.handleInput = function(dt) {
+    if(dt =='up' && this.y >= 50) {
+        this.y -= 50;
+    }
+    else if(dt == 'down' && this.y <= 350) {
+        this.y += 50;
+    }
+    else if(dt == 'right' && this.x <= 350) {
+        this.x += 50;
+    }
+    else if(dt == 'left' && this.x >= 50) {
+        this.x -= 50;
+    }
+    if(this.y <= 5 && key.hasKey == true) {
+        var modal = document.getElementById('myModal');
+        var header = document.getElementsByClassName("modal-header")[0];
+        modal.style.display = "block";
+        header.style.background = "#FFA500";
+        modal.style.background = "url('../images/laurel-wreath.png') 50% 0% no-repeat";
+        modal.style.backgroundSize = "1000px 700px";
+        document.getElementById('warn').innerHTML = "Winner!";
+        document.getElementById('modal-msg').innerHTML = "Congratulation! You have finished the game with:";
+        document.getElementById('win-info').style.display = "block";
+        document.getElementById("modal-lives").innerHTML = player.lives;
+        document.getElementById("modal-hrt").innerHTML = document.getElementById("hrt").textContent;
+        document.getElementById("modal-key").innerHTML = document.getElementById("key").textContent;
+        document.getElementById("modal-gem").innerHTML = document.getElementById("gem").textContent;
+    }
+    else if(key.hasKey == false && this.y <= 10) {
+        var modal = document.getElementById('myModal');
+        modal.style.display = "block";
+        document.getElementById('warn').innerHTML = "Warning!";
+        document.getElementById('modal-msg').innerHTML = "You have to get the key to finish the game!";
+    }
+    else {
+        console.log("Reached the end of "+dt+" side.");
+    }
+};
+
+//Create gem object 
+var Gem = function() {
+    this.spirit = "images/gem-orange.png";
+    this.x = 100;
+    this.y = 85;
+    this.gemBackGrd = "";
+    this.remove = false;
+    this.hasGem = false;
+};
+
+//create gem object
+var allGems = new Gem();
+
+//Removing the object after player collides with it
+Gem.prototype.getGem = function (){
+    this.remove = true;
+    return true;
+}
+
+Gem.prototype.render = function() {
+    //store the image space before drawing the gem to redraw when needed
+    this.gemBackGrd = ctx.getImageData(this.x, this.y,100,140);
+    ctx.drawImage(Resources.get(this.spirit), this.x, this.y,100,140);
+};
+
+Gem.prototype.checkCollisions = function () {
+    if(((Math.floor(player.x) >= Math.floor(allGems.x) - 20) && (Math.floor(player.x) <= Math.floor(allGems.x) + 20)) && ((Math.floor(player.y) >= Math.floor(allGems.y) - 20) && (Math.floor(player.y) <= Math.floor(allGems.y) + 20))) {
+        this.getGem(); 
+    }  
+};
+
+var info = document.getElementsByClassName("fa-info-circle")[0];
+info.addEventListener("click",function() {
+    var modal = document.getElementById('infoModal');
+    modal.style.display = "block";
+});
+
+//create key object
+var Key = function() {
+    this.spirit = "images/Key.png";
+    this.x = 300;
+    this.y = 340;
+    this.keyBackgrd = "";
+    this.remove = false;
+    this.hasKey = false;
+};
+
+Key.prototype.render = function() {
+    //store the key space area before drawing the key
+    this.keyBackgrd = ctx.getImageData(this.x, this.y,100,140);
+    ctx.drawImage(Resources.get(this.spirit), this.x, this.y,100,140);
+};
+
+//Removing the object after player collides with it
+Key.prototype.getKey = function (){
+    this.remove = true;
+    return true;
+}
+
+var key = new Key();
+
+Key.prototype.checkCollisions = function () {
+    if(((Math.floor(player.x) >= Math.floor(key.x) - 20) && (Math.floor(player.x) <= Math.floor(key.x) + 20)) && ((Math.floor(player.y) >= Math.floor(key.y) - 20) && (Math.floor(player.y) <= Math.floor(key.y) + 20))) {
+        this.getKey(); 
+    }  
+};
+
+var Heart = function() {
+    this.spirit = "images/Heart.png";
+    this.x = 400;
+    this.y = 420;
+    this.hrtBackgrd = "";
+    this.remove = false;
+    this.hasHrt = false;
+};
+
+Heart.prototype.render = function() {
+    this.hrtBackgrd = ctx.getImageData(this.x, this.y,100,140);
+    ctx.drawImage(Resources.get(this.spirit ), this.x, this.y,100,140);
+};
+
+//Removing the object after player collides with it
+Heart.prototype.getHrt = function (){
+    this.remove = true;
+    return true;
+}
+
+var heart = new Heart();
+Heart.prototype.checkCollisions = function () {
+    if(((Math.floor(player.x) >= Math.floor(heart.x) - 20) && (Math.floor(player.x) <= Math.floor(heart.x) + 20)) && ((Math.floor(player.y) >= Math.floor(heart.y) - 20) && (Math.floor(player.y) <= Math.floor(heart.y) + 20))) {
+            this.getHrt();
+    }  
+};
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    location.reload();
+    modal.style.display = "none";
+}
+
+var infoModal = document.getElementById('infoModal');
+var span = document.getElementsByClassName("info-close")[0];
+window.onclick = function(event) {
+    if (event.target == infoModal) {
+        infoModal.style.display = "none";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    infoModal.style.display = "none";
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+var allEnemies = [new Enemy(0,145,50),new Enemy(0,60,100),new Enemy(0,225,100),new Enemy(0,145,200),new Enemy(0,225,50),new Enemy(0,60,150)];
+
+//player collision and player lives value check.
+Player.prototype.checkCollisions = function () {
+    allEnemies.forEach(function(enemy) {
+        //if the player has sufficient lives and to check for collision co ordinated of enemy and player
+        if(player.lives >= 1) {
+            if(((Math.floor(enemy.x) >= Math.floor(player.x) - 40) && (Math.floor(enemy.x) <= Math.floor(player.x) + 40)) && ((Math.floor(enemy.y) >= Math.floor(player.y) - 40) && (Math.floor(enemy.y) <= Math.floor(player.y) + 40))) {
+                player.y = 350;
+                player.lives--;
+                document.getElementById("lives").innerHTML = player.lives;
+            }  
+        }
+        else if(player.lives == 0){
+            document.getElementById("lives").innerHTML = player.lives;
+            var modal = document.getElementById('myModal');
+            modal.style.display = "block";
+            document.getElementById('warn').innerHTML = "Warning!";
+            document.getElementById('modal-msg').innerHTML = "Sorry! You do not have sufficient player life to continue! Try again!";
+          }
+    });
+};
+
+//events 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -41,6 +249,8 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+
--- js/engine.js
+++ js/engine.js
@@ -22,12 +22,13 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,hide;
 
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
+    
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
@@ -57,7 +58,7 @@ var Engine = (function(global) {
          */
         win.requestAnimationFrame(main);
     }
-
+  
     /* This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
@@ -67,7 +68,8 @@ var Engine = (function(global) {
         lastTime = Date.now();
         main();
     }
-
+    
+   
     /* This function is called by main (our game loop) and itself calls all
      * of the functions which may need to update entity's data. Based on how
      * you implement your collision detection (when two entities occupy the
@@ -79,7 +81,10 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        player.checkCollisions();
+        key.checkCollisions();
+        heart.checkCollisions();
+        allGems.checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -112,14 +117,15 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/grass-block.png',
             ],
             numRows = 6,
             numCols = 5,
+          
             row, col;
         
         // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.clearRect(0,0,canvas.width,canvas.height);
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
@@ -135,9 +141,9 @@ var Engine = (function(global) {
                  * we're using them over and over.
                  */
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+              
             }
         }
-
         renderEntities();
     }
 
@@ -152,31 +158,65 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
         player.render();
+        key.render();
+        heart.render();
+        allGems.render();
+        //when player collides with object obj.remove is set true and the image stored before is added
+        if(key.remove) {
+            key.hasKey = true;
+            document.getElementById("key").innerHTML = 1;
+            ctx.putImageData(key.keyBackgrd,key.x,key.y);
+        }
+        if(heart.remove) {
+            heart.hasHrt = true;
+            document.getElementById("hrt").innerHTML = 1;
+            ctx.putImageData(heart.hrtBackgrd,heart.x,heart.y);
+        }
+        if(allGems.remove) {
+            allGems.hasGem = true;
+            document.getElementById("gem").innerHTML = 1;
+            ctx.putImageData(allGems.gemBackGrd,allGems.x,allGems.y);
+        }
     }
-
+    
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
-    }
-
+        this.lives = 10;
+        document.getElementById("lives").innerHTML = this.lives;
+        document.getElementById("hrt").innerHTML = 0;
+        document.getElementById("key").innerHTML = 0;
+        document.getElementById("gem").innerHTML = 0;
+    };
+    
+    var ele = document.getElementById("refresh");
+    ele.addEventListener("click" , function() {
+        //refresh 
+        location.reload();  
+    });
+    
+    
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
-    ]);
+            'images/stone-block.png',
+            'images/water-block.png',
+            'images/grass-block.png',
+            'images/enemy-bug.png',
+            'images/char-cat-girl.png',
+            'images/char-horn-girl.png',
+            'images/Key.png',
+            'images/Heart.png',
+            'images/gem-green.png',
+            'images/gem-orange.png',
+            'images/Rock.png'
+        ]);
     Resources.onReady(init);
-
     /* Assign the canvas' context object to the global variable (the window
      * object when run in a browser) so that developers can use it more easily
      * from within their app.js files.
